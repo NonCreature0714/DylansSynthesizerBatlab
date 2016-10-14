37659a88465777cc338db0d095bedadffe4be015
Bill <brubaker.bill@gmail.com>
2016-10-13 19:40:22 -0500

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <string>
 
+/*
+ *
+ * This small code sample is for the purpose of demonstrating enums
+ * in the Makeraspace which I work.
+ *
+ * Vectors are not used because the student was not familiar with
+ * vectors and their requirement is that vectors are not permitted.
+ *
+ * This code makes little sense because it was used in a classroom
+ * setting to demonstrate what works and what doesn't.
+ */
 
 
 using namespace std;
@@ -17,9 +28,10 @@ enum Scale {
     Tonic=1, Second, Third, Forth, Fifth, Sixth, Seventh
 };
 
-Scale chort[]={Tonic, Third, Sixth};
+Scale chordArray[]={Tonic, Third, Sixth}; //example of array of Scale type.
 
 /*
+ * Example that these declaration are the as enums.
 int Tonic = 1;
 int Second = 2;
 int Third = 3;
@@ -28,7 +40,7 @@ int Third = 3;
 int main() {
 
     int numNotes = 0;
-    int *chord;
+    int *chordPtr;
 
     cout << "A Sharp: " << Ab << endl;
 
@@ -42,16 +54,18 @@ int main() {
 
     cout << "Number of notes: " << numNotes << endl;
 
-    chord[numNotes]={0};
-
-    chord[0] = Bc;
-    chord[1] = C;
-    chord[2] = A;
     cout << endl;
-    cout << "Chord is: ";
+
+    chordPtr[numNotes] = {0};
+
+
+    chordPtr[0] = Bc;
+    chordPtr[1] = C;
+    chordPtr[2] = A;
+
 
     for (int i=0; i<numNotes;i++)
-        cout << chord[i] << " ";
+        cout << chordPtr[i] << " ";
 
 
     return 0;
